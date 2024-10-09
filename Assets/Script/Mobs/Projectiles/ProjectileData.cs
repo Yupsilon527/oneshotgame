1f456ca5356449cbfc48e41f48dde22d3c764b58
Yupsilon <paul.sendroiu@gmail.com>
2024-10-09 13:32:41 +0300

    Push

--- Assets/Script/Mobs/Projectiles/ProjectileData.cs
+++ Assets/Script/Mobs/Projectiles/ProjectileData.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ProjectileData", menuName = "BulletHell/ProjectileData")]
 public class ProjectileData : ScriptableObject
 {
-    public Color color; public float ForwardSpeed; public float RightSpeed; public float Life; public float Scale; public float Damage; public Projectile.MoveType Behavior;
+    public Color color; public float ForwardSpeed; public float RightSpeed; public float Life; public float Scale; public float Damage; public Projectile.MoveType Behavior; public bool oosBounce = false;
 }
